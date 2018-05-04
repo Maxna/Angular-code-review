01814ebfa04cfb7d07edd766f3b1d4b8132673fb
Matt Nardoni <nardoni59@gmail.com>
2018-05-04 15:54:08 -0700

    Revise Project functionality, add html, add projects to master project list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,13 @@ export class AppComponent {
   title = 'Freelancers';
   header: string = "Hire Freelancers Online";
   masterProjectList: Project[] = [
-    new Project("Website Development", "Project", "Starting at $99/hr")
-    new Project("Logo Design", "Contest", "Starting at $149/hr")
+    new Project("Website Development", "Project", "Starting from $99/hr"),
+    new Project("Web Design", "Contest", "Starting from $90/hr"),
+    new Project("SEO Marketing", "Project", "Starting from $100/hr"),
+    new Project("Mobile App", "Project", "Starting from $200/hr"),
+    new Project("Article Writing", "Project", "Starting from $50/hr"),
+    new Project("Business Card", "Contest", "Starting from $30/hr"),
+    new Project("Logo Design", "Contest", "Starting from $150/hr"),
+    new Project("Data Entry", "Project", "Starting from $550/hr")
   ];
 }
